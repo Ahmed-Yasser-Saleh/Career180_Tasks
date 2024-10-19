393d003d6153df0229b663b47a7d9d3af90aa69c
Ahmed-Yasser-Saleh <ahmedyasserr552@gmail.com>
2024-10-19 22:24:04 +0300

    Task_8  Dictionary<Question, List<Answer>>

---
+++ Task_8/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Task_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<Question, List<Answer>> myquestion = new Dictionary<Question, List<Answer>>()
+            {
+                { new Question("Which is not vegetable",2),new List<Answer>()
+                    { 
+                      new Answer(1, "Apple"),
+                      new Answer(2, "Car"),
+                      new Answer(3, "orange"),
+                      new Answer(4, "banana")
+                    }
+                },  
+
+                { new Question("Which is not color"),new List<Answer>()
+                   {
+                     new Answer(1, "Blue"),
+                     new Answer(2, "Red"),
+                     new Answer(3, "orange"),
+                     new Answer(4, "ahmed")
+                   } 
+                }
+            };
+            foreach (var question in myquestion.Keys)
+            {
+                Console.WriteLine(question);
+                foreach (var answer in myquestion[question])
+                {
+                    Console.WriteLine(answer);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    public class Question
+    {
+        public string question { get; set; }
+        public int mark { get; set; }
+        public Question(string Question, int Mark = 1)
+        {
+            question = Question;
+            mark = Mark;
+        }
+        public override string ToString()
+        {
+            return $"{question}                     {mark} marks";
+        }
+    }
+    public class Answer
+    {
+        public int num { get; set;}
+        public string  choose { get; set; }
+        public Answer(int Num, string Choose) {
+            num = Num;
+            choose = Choose;
+        }
+        public override string ToString()
+        {
+            return $"{num}. {choose}";
+        }
+    }
+}
