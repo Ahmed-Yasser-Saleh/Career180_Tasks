a5c43239bea8b3a67d340cd7212ecd8514ed1cde
Ahmed-Yasser-Saleh <ahmedyasserr552@gmail.com>
2024-11-06 16:18:02 +0200

    wep_api1_test

---
+++ Task_16 WebApi/Controllers/CoursesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+using Task_16_WebApi.Model;
+
+namespace Task_16_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        List<Course> courses = new List<Course>
+        {
+            new Course(1, "Mathematics", 40, "Active"),
+            new Course(2, "Physics", 35, "Inactive"),
+            new Course(3, "Chemistry", 30, "Active")
+        };
+        /* [
+           {
+               "id": 1,
+               "name": "Mathematics",
+               "duration": 40,
+               "status": "Active"
+           },
+           {
+               "id": 2,
+               "name": "Physics",
+               "duration": 35,
+               "status": "Inactive"
+           },
+           {
+               "id": 3,
+               "name": "Chemistry",
+               "duration": 30,
+               "status": "Active"
+           }
+          ]
+         */
+        [HttpGet]
+        public List<Course> GetAll() {
+        return courses;
+        }
+        [HttpGet("{id}")]
+        public Course Getbyid(int id) {
+        var cs = courses.Where(cs => cs.Id == id).FirstOrDefault();
+            if (cs != null)
+                return cs;
+            return new Course(0, "null", 0, "inactive");
+        }
+    }
+}
---
+++ Task_16 WebApi/Model/Course.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_16_WebApi.Model
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public string Status { get; set; } // "Active", "Inactive"
+
+        public Course(int id, string name, int duration, string status)
+        {
+            Id = id;
+            Name = name;
+            Duration = duration;
+            Status = status;
+        }
+        public override string ToString()
+        {
+            return $"name: {Name}, id: {Id}, Duration: {Duration}, status: {Status}";
+        }
+    }
+}
